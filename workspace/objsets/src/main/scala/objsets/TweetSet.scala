798d6992b7e12aa1050d23a040209a3c4371ef5c
Gleb Dolgich <glebd@me.com>
2013-04-13 18:32:44 +0100

    Implement trending

--- workspace/objsets/src/main/scala/objsets/TweetSet.scala
+++ workspace/objsets/src/main/scala/objsets/TweetSet.scala
@@ -224,14 +224,10 @@ object GoogleVsApple {
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
-  println("\nGoogle tweets")
-  GoogleVsApple.googleTweets foreach println
-  println("\nApple tweets")
-  GoogleVsApple.appleTweets foreach println
   // Print the trending tweets
-  //GoogleVsApple.trending foreach println
+  GoogleVsApple.trending foreach println
 }
