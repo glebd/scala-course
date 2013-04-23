1a1c504ce1fc73435c31cc6a593937c258ec1a61
Gleb Dolgich <glebd@me.com>
2013-04-23 01:17:11 +0100

    Week 4 video exercises

---
+++ workspace/hello-project/src/week4/Expr.scala
@@ -0,0 +1,7 @@
+package week4
+
+trait Expr
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Prod(e1: Expr, e2: Expr) extends Expr
+case class Var(name: String) extends Expr
