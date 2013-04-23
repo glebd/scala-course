25e556d36cdbcbaa34c3eb82347eced1d9881ee1
Gleb Dolgich <glebd@me.com>
2013-04-23 03:33:16 +0100

    Week 4 WIP

--- workspace/patmat/src/main/scala/patmat/Huffman.scala
+++ workspace/patmat/src/main/scala/patmat/Huffman.scala
@@ -77,7 +77,12 @@ object Huffman {
    *       println("integer is  : "+ theInt)
    *   }
    */
-  def times(chars: List[Char]): List[(Char, Int)] = ???
+  def times(chars: List[Char]): List[(Char, Int)] = chars match {
+    case Nil => List()
+    case c :: cs => {
+      val pair: (Char, Int) = (c, 1)
+    }
+  }
 
   /**
    * Returns a list of `Leaf` nodes for a given frequency table `freqs`.
