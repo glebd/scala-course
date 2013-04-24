0504c360ea95d13cc8264b36cb33bb7bfcef416d
Gleb Dolgich <gdolgich@riskcare.com>
2013-04-24 17:36:34 +0100

    Implement `singleton` method

--- workspace/patmat/src/main/scala/patmat/Huffman.scala
+++ workspace/patmat/src/main/scala/patmat/Huffman.scala
@@ -100,7 +100,7 @@ object Huffman {
   /**
    * Checks whether the list `trees` contains only one single code tree.
    */
-  def singleton(trees: List[CodeTree]): Boolean = ???
+  def singleton(trees: List[CodeTree]): Boolean = trees.size == 1
 
   /**
    * The parameter `trees` of this function is a list of code trees ordered
