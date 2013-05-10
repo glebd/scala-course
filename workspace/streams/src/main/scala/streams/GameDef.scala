7165176dcf6afb9539a333f63e8e88af01e7f9b5
Gleb Dolgich <gdolgich@riskcare.com>
2013-05-10 15:12:04 +0100

    Implement startPos

--- workspace/streams/src/main/scala/streams/GameDef.scala
+++ workspace/streams/src/main/scala/streams/GameDef.scala
@@ -84,7 +84,7 @@ trait GameDef {
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = Block(startPos, startPos)
 
   /**
    * A block is represented by the position of the two cubes that
