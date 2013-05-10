ec8b8714c2585527e64d8d3dafd4bab4abf81279
Gleb Dolgich <gdolgich@riskcare.com>
2013-05-10 15:09:33 +0100

    Implement isLegal

--- workspace/streams/src/main/scala/streams/GameDef.scala
+++ workspace/streams/src/main/scala/streams/GameDef.scala
@@ -150,7 +150,7 @@ trait GameDef {
     /**
      * Returns `true` if the block is entirely inside the terrain.
      */
-    def isLegal: Boolean = ???
+    def isLegal: Boolean = terrain(b1) && terrain(b2)
 
   }
 }
