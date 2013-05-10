18e8838377f476047dda85d0d2fdb0d2868ad930
Gleb Dolgich <gdolgich@riskcare.com>
2013-05-10 15:29:11 +0100

    Implement legalNeighbors

--- workspace/streams/src/main/scala/streams/GameDef.scala
+++ workspace/streams/src/main/scala/streams/GameDef.scala
@@ -140,7 +140,7 @@ trait GameDef {
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = neighbors.filter(x => x._1.isLegal)
 
     /**
      * Returns `true` if the block is standing.
