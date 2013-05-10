5e5d34a81896cedc27b71c8b43f9fc80d9a14754
Gleb Dolgich <gdolgich@riskcare.com>
2013-05-10 16:06:05 +0100

    Implement newNeighborsOnly

--- workspace/streams/src/main/scala/streams/Solver.scala
+++ workspace/streams/src/main/scala/streams/Solver.scala
@@ -16,15 +16,15 @@ trait Solver extends GameDef {
    * This function takes two arguments: the current block `b` and
    * a list of moves `history` that was required to reach the
    * position of `b`.
-   * 
+   *
    * The `head` element of the `history` list is the latest move
    * that was executed, i.e. the last move that was performed for
    * the block to end up at position `b`.
-   * 
+   *
    * The function returns a stream of pairs: the first element of
    * the each pair is a neighboring block, and the second element
    * is the augmented history of moves required to reach this block.
-   * 
+   *
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
@@ -38,33 +38,37 @@ trait Solver extends GameDef {
    * positions that have already been explored. We will use it to
    * make sure that we don't explore circular paths.
    */
-  def newNeighborsOnly(neighbors: Stream[(Block, List[Move])], explored: Set[Block]): Stream[(Block, List[Move])] = ???
+  def newNeighborsOnly(neighbors: Stream[(Block, List[Move])], explored: Set[Block]): Stream[(Block, List[Move])] =
+    for {
+      (b, m) <- neighbors
+      if (!explored.contains(b))
+    } yield (b, m)
 
   /**
    * The function `from` returns the stream of all possible paths
    * that can be followed, starting at the `head` of the `initial`
    * stream.
-   * 
+   *
    * The blocks in the stream `initial` are sorted by ascending path
    * length: the block positions with the shortest paths (length of
    * move list) are at the head of the stream.
-   * 
+   *
    * The parameter `explored` is a set of block positions that have
    * been visited before, on the path to any of the blocks in the
    * stream `initial`. When search reaches a block that has already
    * been explored before, that position should not be included a
    * second time to avoid cycles.
-   * 
+   *
    * The resulting stream should be sorted by ascending path length,
    * i.e. the block positions that can be reached with the fewest
    * amount of moves should appear first in the stream.
-   * 
+   *
    * Note: the solution should not look at or compare the lengths
    * of different paths - the implementation should naturally
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+    explored: Set[Block]): Stream[(Block, List[Move])] = ???
 
   /**
    * The stream of all paths that begin at the starting block.
