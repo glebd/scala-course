ce76595f1ef428d4b7a1ccf67c8a0c937efae4ac
Gleb Dolgich <gdolgich@riskcare.com>
2013-05-10 15:31:52 +0100

    Implement done

--- workspace/streams/src/main/scala/streams/Solver.scala
+++ workspace/streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.b1 == goal && b.b2 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
