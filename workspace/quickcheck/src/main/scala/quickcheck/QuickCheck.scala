8709f340f47932c96a7410505b5554ed47a5c5bd
Gleb Dolgich <glebd@me.com>
2013-11-12 00:10:22 +0000

    Add property gen1

--- workspace/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ workspace/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -21,4 +21,8 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
 }
