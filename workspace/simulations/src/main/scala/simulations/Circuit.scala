1ed2561da369256593dd0c5da0a054cb0393ea16
Gleb Dolgich <glebd@me.com>
2013-11-16 17:59:24 +0000

    Implement orGate

--- workspace/simulations/src/main/scala/simulations/Circuit.scala
+++ workspace/simulations/src/main/scala/simulations/Circuit.scala
@@ -59,7 +59,13 @@ abstract class CircuitSimulator extends Simulator {
   //
 
   def orGate(a1: Wire, a2: Wire, output: Wire) {
-    ???
+    def orAction() {
+      val a1Sig = a1.getSignal
+      val a2Sig = a2.getSignal
+      afterDelay(OrGateDelay) { output.setSignal(a1Sig | a2Sig) }
+    }
+    a1 addAction orAction
+    a2 addAction orAction
   }
   
   def orGate2(a1: Wire, a2: Wire, output: Wire) {
