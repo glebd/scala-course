8936dd62cb7955b2ab644b540ef426e0fd9b9816
Gleb Dolgich <glebd@me.com>
2013-11-17 22:21:43 +0000

    Implement lifecycle events

--- workspace/simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ workspace/simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -10,13 +10,20 @@ class EpidemySimulator extends Simulator {
     val population: Int = 300
     val roomRows: Int = 8
     val roomColumns: Int = 8
-
-    // to complete: additional parameters of simulation
+    
+    val prevalence = 0.01
+    val incubation = 6
+    val death = 14
+    val immunity = 16
+    val convalescence = 18
+    val mortality = 0.25
+    val transmissibility = 0.4
+    val daysBeforeMove = 5
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (for (i <- 0 to population) yield new Person(i)).toList
 
   class Person (val id: Int) {
     var infected = false
@@ -28,8 +35,52 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    if ((id + 1) % 100 == 0)
+      becomeInfected
+    
+    afterDelay(1)(mode)
+    
+    def mode() {
+      val days = randomBelow(daysBeforeMove - 1) + 1
+      afterDelay(days)(move)
+    }
+    
+    def move {
+      
+    }
+    
+    def becomeInfected() {
+      if (immune || dead || sick) return
+      infected = true
+      afterDelay(incubation)(becomeSick)
+      if (randomBelow(100) <= mortality*100)
+        afterDelay(death)(die)
+      else {
+        afterDelay(immunity)(becomeImmune)
+        afterDelay(convalescence)(becomeHealthy)
+      }
+    }
+    
+    def becomeSick() {
+      if (!infected) return
+      sick = true
+    }
+    
+    def die() {
+      if (!sick) return
+      dead = true
+    }
+    
+    def becomeImmune() {
+      if (!sick) return
+      immune = true
+    }
+    
+    def becomeHealthy() {
+      if (sick) return
+      sick = false
+      immune = false
+      infected = false
+    }
   }
 }
