784f087d8d3485b5f0a2656f1e7819b3db7bb72c
Gleb Dolgich <glebd@me.com>
2013-11-18 01:43:20 +0000

    Tests passing, display is wrong, all getting dead or immune

--- workspace/simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ workspace/simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -35,18 +35,83 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
+    // Initial incidence
     if ((id + 1) % 100 == 0)
       becomeInfected
     
     afterDelay(1)(mode)
     
     def mode() {
+      getInfectedMaybe()
       val days = randomBelow(daysBeforeMove - 1) + 1
       afterDelay(days)(move)
     }
     
+    // Check the room
+    def getInfectedMaybe() {
+      if (infected || sick || dead || immune) return
+      if (roomIsSafe(List(row, col))) return
+      if (randomBelow(100) <= transmissibility*100)
+        afterDelay(0)(becomeInfected)
+    }
+    
+    def prevRow(r: Int): Int = {
+      if (r == 0) roomRows-1
+      else r-1
+    }
+    
+    def prevCol(c: Int): Int = {
+      if (c == 0) roomColumns-1
+      else c-1
+    }
+    
+    def nextRow(r: Int): Int = {
+      if (r == roomRows-1) 0
+      else r+1
+    }
+    
+    def nextCol(c: Int): Int = {
+      if (c == roomColumns-1) 0
+      else c+1
+    }
+    
     def move {
       if (dead) return
+      
+      // determine available rooms
+      val topRoom = List(prevRow(row), col)
+      val rightRoom = List(row, nextCol(col))
+      val bottomRoom = List(nextRow(row), col)
+      val leftRoom = List(row, prevCol(col))
+      
+      val rooms = List(topRoom, rightRoom, bottomRoom, leftRoom)
+      val safeRooms = rooms.filter(roomAppearsSafe)
+      if (safeRooms != Nil) {
+        val roomIndex = randomBelow(safeRooms.length)
+        val room = safeRooms(roomIndex)
+        moveTo(room(0), room(1))
+      }
+      
+      afterDelay(0)(mode)
+    }
+    
+    def moveTo(newrow: Int, newcol: Int) {
+      row = newrow
+      col = newcol
+    }
+    
+    def roomAppearsSafe(coords: List[Int]): Boolean = {
+      val row = coords(0)
+      val col = coords(1)
+      val unsafe = persons.exists(p => {p.sick || p.dead})
+      return !unsafe
+    }
+    
+    def roomIsSafe(coords: List[Int]): Boolean = {
+      val row = coords(0)
+      val col = coords(1)
+      val unsafe = persons.exists(p => {p.infected || p.sick || p.dead})
+      return !unsafe
     }
     
     def becomeInfected() {
