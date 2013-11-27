6ca5a79661abee266f7db9b312c80640fa988a59
Gleb Dolgich <glebd@me.com>
2013-11-27 03:41:50 +0000

    Reformat code

--- workspace/nodescala/src/test/scala/nodescala/tests.scala
+++ workspace/nodescala/src/test/scala/nodescala/tests.scala
@@ -1,14 +1,12 @@
 package nodescala
 
-
-
 import scala.language.postfixOps
-import scala.util.{Try, Success, Failure}
+import scala.util.{ Try, Success, Failure }
 import scala.collection._
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.async.Async.{async, await}
+import scala.async.Async.{ async, await }
 import org.scalatest._
 import NodeScala._
 import org.junit.runner.RunWith
@@ -33,7 +31,7 @@ class NodeScalaSuite extends FunSuite {
       case t: TimeoutException => // ok!
     }
   }
-  
+
   test("A Future should be delayed") {
     @volatile var test = 0
     val delayed = Future.delay(1 second)
