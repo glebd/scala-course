b3a451125aa1efe07472da2d7397d06b41503295
Gleb Dolgich <glebd@me.com>
2013-12-04 03:40:13 +0000

    Reformat code

--- workspace/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ workspace/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -11,14 +11,15 @@ import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
 import rx.lang.scala.Observable
 
-/** Basic facilities for dealing with Swing-like components.
-*
-* Instead of committing to a particular widget implementation
-* functionality has been factored out here to deal only with
-* abstract types like `ValueChanged` or `TextField`.
-* Extractors for abstract events like `ValueChanged` have also
-* been factored out into corresponding abstract `val`s.
-*/
+/**
+ * Basic facilities for dealing with Swing-like components.
+ *
+ * Instead of committing to a particular widget implementation
+ * functionality has been factored out here to deal only with
+ * abstract types like `ValueChanged` or `TextField`.
+ * Extractors for abstract events like `ValueChanged` have also
+ * been factored out into corresponding abstract `val`s.
+ */
 trait SwingApi {
 
   type ValueChanged <: Event
@@ -46,18 +47,20 @@ trait SwingApi {
 
   implicit class TextFieldOps(field: TextField) {
 
-    /** Returns a stream of text field values entered in the given text field.
-      *
-      * @param field the text field
-      * @return an observable with a stream of text field updates
-      */
     def textValues: Observable[String] = ???
 
+    /**
+     * Returns a stream of text field values entered in the given text field.
+     *
+     * @param field the text field
+     * @return an observable with a stream of text field updates
+     */
   }
 
   implicit class ButtonOps(button: Button) {
 
-    /** Returns a stream of button clicks.
+    /**
+     * Returns a stream of button clicks.
      *
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
